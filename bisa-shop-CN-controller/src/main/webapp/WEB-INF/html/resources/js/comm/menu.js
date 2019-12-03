4bbb386b1047825bed2af1a20cf70061e1b8cedc
youge2794 <youge2794@163.com>
2019-12-04 02:11:58 +0800

    2:11

--- bisa-shop-CN-controller/src/main/webapp/WEB-INF/html/resources/js/comm/menu.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    //判断请求的是哪个链接，然后触发点击事件。
-    $(".user-munu").click(function () {
-        $(this).parent().find(".col-active").removeClass("col-active");
-        $(this).addClass("col-active");
-    });
-});
